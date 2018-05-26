65c8ad88adeb8c1d166ddfaa5b42f59a833987b0
田原 一樹 <ssp0727.lnc@gmail.com>
2018-05-26 12:44:25 +0900

    add file

--- sample/2/index.js
+++ sample/2/index.js
@@ -2,13 +2,25 @@ new Vue({
   el:'#app',
   data:{
     counter: 0,
-    title: 3
+    title: 3,
+    x: 0,
+    y: 0,
+    value:''
   },
   methods: {
-    increase: function () {
-      this.counter++;
+    increase: function (step, event) {
+      this.counter += step;
+
       this.title = this.title + this.counter;
       return this.title;
+    },
+    updateCoordinates:function(e) {
+      console.log(e);
+      this.x = e.clientX;
+      this.y = e.clientY;
+    },
+    alertMe: function () {
+      alert('アラート');
     }
   }
 });
