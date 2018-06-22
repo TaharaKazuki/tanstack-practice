c42f6cf2785f7e3c452511c06b4c0497966487b4
tahara-kazuki <tahara-kazuki@dmm.com>
2018-06-22 21:13:53 +0900

    add
    component
    emit
    etc...

---
+++ myproject/src/OnlineFriends.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="online-friends">
+    <h2>online Friends</h2>
+    <div v-for="(friend, index) in friends" :key="index">
+      <span v-if="friend.online">{{ friend.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'OnlineFriends',
+    props: ['friends'],
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
