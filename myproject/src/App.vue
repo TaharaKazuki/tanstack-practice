390a348689611c6ac114d4396c24846242d3e670
田原 一樹 <ssp0727.lnc@gmail.com>
2018-06-24 17:28:00 +0900

    add file life cycle

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,42 +2,27 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend"/>
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
   import Navbar from './NavBar';
-  import AllFriends from './AllFriends';
-  import OnlineFriends from './OnlineFriends';
-
+  import Blogs from './Blogs';
 
   export default {
     name: 'app',
     components: {
       Navbar,
-      AllFriends,
-      OnlineFriends
+      Blogs,
     },
     data () {
       return {
         title: 'Welcome to  Vue.js',
-        friends: [
-          { name: 'Mario', online: true },
-          { name: 'Tom', online: false },
-          { name: 'Bob', online: true },
-          { name: 'Joy', online: false },
-        ]
       }
     },
     methods: {
-      deleteFriend(payload){
-        console.log(payload);
-        this.friends = this.friends.filter(friend => {
-          return friend.name !== payload.name
-        });
-      }
+
     }
   }
 </script>
---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+    <button @click="changeTitle">Change Title</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Blog',
+    data(){
+      return {
+        blogTitle:'Blogs'
+      }
+    },
+    methods: {
+      changeTitle(){
+        this.blogTitle = 'Amazing';
+      }
+    },
+    beforeCreate(){
+      alert('before Create');
+    },
+    created(){
+      alert('created');
+    },
+    beforeUpdate(){
+      alert('beforeUpdate');
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
